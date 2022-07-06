e32649f0f3aa9cbecd58a8d0f22f92f973659db5
MoonHKLee <mf311110@gmail.com>
2022-07-06 22:55:30 +0900

    Initial commit

---
+++ src/main/java/com/example/givememyseat/GiveMeMySeatApplication.java
@@ -0,0 +1,13 @@
+package com.example.givememyseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveMeMySeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveMeMySeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/givememyseat/GiveMeMySeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.givememyseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveMeMySeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
